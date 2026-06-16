d7c26e419f2b3c9cd3394b16a4ff05ceca1b4255
CatalinaPolanco <catalina.polanco@rhacc.ac.uk>
2026-06-16 09:54:48 +0000

    refactor code

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ def generate_summary(total):
   return f"Summary: the final total is £{total}" 
 price = 5 
 quantity = 3 
-total = calculate_total(price, quantity) display_total(total) print(generate_summary(total))
+total = calculate_total(price, quantity) display_total(total) 
+print(generate_summary(total))
